e21dc79af1eed1db64e611ae623f03a725e2ed18
DokiDokiDesu <demre421423@hotmail.com>
2026-02-02 02:18:38 +0300

    setting for github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/sunum-website-annem/", // github pages setting
   plugins: [react(), tailwindcss()],
 });
