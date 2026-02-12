204166a3a3c56947243390d7fc8d37913879d4f5
DokiDokiDesu <demre421423@hotmail.com>
2026-02-13 00:01:29 +0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
       output: {
         entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
         chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
-        assetFileNames: `assets/[name]-[hash].[ext]`
-      }
-    }
-  }
+        assetFileNames: `assets/[name]-[hash].[ext]`,
+      },
+    },
+  },
 });
